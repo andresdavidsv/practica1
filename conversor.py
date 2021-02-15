46fb1322b83dca32176e0e2a68f58fab5fd5d3fc
Andres David Solarte Vidal <andresdavidsolartevidal@gmail.com>
2021-02-15 14:42:44 -0500

    first tutorial py

---
+++ conversor.py
@@ -0,0 +1,21 @@
+# # Conversor Pesos Col a Dolares
+# pesos = input("¿Cuantos pesos colombianos tienes?: ")
+# pesos = float(pesos)
+# valor_dolar = 3875
+# dolares = pesos / valor_dolar
+# dolares = round(dolares,2)
+# dolares = str(dolares)
+# print(f"Tienes ${dolares} dolares")
+
+# # Conversor Dolares a Pesos Col
+# dolares = input("¿Cuantos dolares tienes?: ")
+# dolares = float(dolares)
+# valor_dolar = 3875
+# pesos = dolares * valor_dolar
+# pesos = int(pesos)
+# pesos = str(pesos)
+# print(f"Tienes ${pesos} pesos")
+
+Regalo = 'Ciencunta'*500000
+print(f"TE AMO YULIANA")
+print(f"${Regalo} pesos")
